4261bf5a84ceb31e4ec8c5c796c95418a6e5eebf
Florian Thöni <florian.thoni@teamwork.net>
2017-05-20 21:51:04 +0200

    Replace by using markdown comment

--- replace.py
+++ replace.py
@@ -10,13 +10,14 @@ def replace(original_file, placeholder_start, placeholder_end,
     placeholder and ending placeholder by the
     content of replace_file. It will save it as target_file if provided, or
     in original_file otherwise.
+    No backslash must be used in the placeholders except in order to escape
+    Result will be put in triple backquotes.
     :param original_file Source that must be modified:
     :param placeholder_start String starting the position to replace:
     :param placeholder_start String ending the position to replace:
     :param replace_file File to include at placeholder position:
     :param target_file Destination where the complete file must be stored.
     Facultative, will be replaced by original_file if not provided:
-    No backslash must be used in the placeholders except in order to escape
     :return:
     """
     if target_file is None:
@@ -27,7 +28,7 @@ def replace(original_file, placeholder_start, placeholder_end,
 
     with open(replace_file, 'r') as my_replace_file:
         #we keep placeholders to be able to replay the process several times
-        replace_content = placeholder_start.replace('\\','') + "\n" + my_replace_file.read() + "\n" + placeholder_end.replace('\\','')
+        replace_content = placeholder_start.replace('\\','') + "\n\n```\n" + my_replace_file.read() + "\n```\n\n" + placeholder_end.replace('\\','')
 
     regex = placeholder_start + r'(.*)' + placeholder_end
     new_content = re.sub(regex, replace_content, original_content, flags=re.DOTALL)
@@ -36,5 +37,5 @@ def replace(original_file, placeholder_start, placeholder_end,
         my_target_file.write(new_content)
 
 print("Will start replace")
-replace('content/pages/wiki/devenir-membre.md', '\[//\]: # BeginLicense', '\[//\]: # EndLicense', 'content/files/licence_g1.txt')
+replace('content/pages/wiki/devenir-membre.md', '\[//\]: # \(BeginLicense\)', '\[//\]: # \(EndLicense\)', 'content/files/licence_g1.txt')
 print("Replace completed")
